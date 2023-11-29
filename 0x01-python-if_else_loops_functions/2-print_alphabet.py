c22ccc2005619dccefb98098c7841792eafa9b38
Therese-Claire <awonguagabi@gmail.com>
2023-11-28 18:58:02 -0800

    printing ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("".join('{}' for _ in range(26)).format \
-        (*map(chr, range(97, 123))), end='')
+print("".join('{}' for _ in range(26)).format(
+    *map(chr, range(97, 123))), end='')
