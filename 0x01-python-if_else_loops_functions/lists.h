ee59e4cef64857692ef8b11a3c2f574da4a88d31
Therese-Claire <awonguagabi@gmail.com>
2023-11-29 18:35:58 -0800

    function that creates a copy of the string, removing the character at the position n

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -8,4 +8,6 @@ def add(a, b):
 def pow(a, b):
 def fizzbuzz():
 listint_t *insert_node(listint_t **head, int number);
+def remove_char_at(str, n):
+def magic_calculation(a, b, c):
 #endif /* LISTS_H */
