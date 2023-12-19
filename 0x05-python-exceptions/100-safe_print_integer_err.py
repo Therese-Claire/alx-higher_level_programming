542d00c9b975910dc0d06f605f852e4579f93405
Therese-Claire <awonguagabi@gmail.com>
2023-12-18 17:42:28 -0800

    :prints in stderr the error precede by Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value), end="")
-        print()
+        print("{:d}".format(value))
         return True
-    except Exception as ex:
-        print("Exception:", ex, file=sys.stderr)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         return False
