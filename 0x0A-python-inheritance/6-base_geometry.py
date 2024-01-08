4b74431a1b1b995d1c1ac19178e58ad2257d7c76
Therese-Claire <awonguagabi@gmail.com>
2024-01-08 14:52:22 -0800

    added instance method to class of task 5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry with public intance method"""
+
+class BaseGeometry:
+    """Class BaseGeometry.
+    """
+    def area(self):
+        """Area function.
+
+
+        Raises:
+        Exception: if area is not implemented.
+        """
+        raise Exception("area() is not implemented")
