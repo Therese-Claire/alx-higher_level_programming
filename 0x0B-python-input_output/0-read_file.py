8adb36e513eb12e7d187ee59e4dd5348956c0326
Therese-Claire <awonguagabi@gmail.com>
2024-01-09 10:56:18 -0800

    reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Defines a function read_file(filename="")"""
+"""Defines the function read_file"""
 
 
 def read_file(filename=""):
-    """function that reads a text file (UTF8),
-    and prints it to stdout
+    """reads a text file and prints to stdout.
 
 
     Args:
