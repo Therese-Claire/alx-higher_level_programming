9e56f9cfd141a8f5fa7f25243813ba89d9fa7166
Therese-Claire <awonguagabi@gmail.com>
2024-03-28 09:36:25 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,7 @@ Write a Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
 import urllib.request
-import urllib.error.HTTPError
+import urllib.error
 import sys
 
 
